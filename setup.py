f8a6126451a9b8f370a458f32520ecc5bca7f00e
praveen yalal <praveen.yalal@trupik.com>
2020-08-02 03:11:54 +0530

    updated the setup details

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='cvutils',
     version='0.1',
     description='Opencv utilities',
-    url='#',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/praveen2357/cvutils",
     author='praveen',
     author_email='praveen2357@gmail.com',
     license='MIT',
-    packages=['cvutils'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     zip_safe=False
 )
 
