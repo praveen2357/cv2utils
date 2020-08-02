c003f1493b16a7c0064aff45436cb0852b627ea3
praveen yalal <praveen.yalal@trupik.com>
2020-08-02 05:50:17 +0530

    resolved issue

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@
  */
 """
 
+import setuptools
 from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='cvutils',
-    version='0.1',
+    version='0.0.1',
     description='Opencv utilities',
     long_description=long_description,
     long_description_content_type="text/markdown",
